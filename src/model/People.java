410d6f4a65008031a82f1fcadbfb126ac89509cf
Neelim Novo <neelim98@gmail.com>
2019-11-30 15:28:49 -0800

    # commit novo

--- src/model/People.java
+++ src/model/People.java
@@ -1,4 +1,54 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class People {
+
+    private String name;
+    private List<String> orderItems;
+    private GroupAdmin groupAdmin;
+
+
+    public People(String name, boolean groupAdminStatus) {
+
+        this.name = name;
+        orderItems = new ArrayList<>();
+        if (groupAdminStatus) {
+            groupAdmin = new GroupAdmin;
+        }
+
+    }
+
+
+    public void addOrderItems {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<String> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public GroupAdmin getGroupAdmin() {
+        return groupAdmin;
+    }
+
+    public void setGroupAdmin(GroupAdmin groupAdmin) {
+        this.groupAdmin = groupAdmin;
+    }
+
+
+
 }
